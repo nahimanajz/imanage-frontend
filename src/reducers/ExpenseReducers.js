28166050b7326761351e299d084657d61245392a
nahimanajz <jazzonahimana@gmail.com>
2021-01-27 22:39:59 +0200

    deploy the app

--- src/reducers/ExpenseReducers.js
+++ src/reducers/ExpenseReducers.js
@@ -1,24 +1,29 @@
 
 import * as ec from '../constants/ExpenseConstants';
-const initialState = { expenses: [], loading:false, error:null, expenseLoading: false, expensesError: null};
 
-export default (state = initialState, action) => {
+const initialExpenses = { expenses: [], loading:false, error:null, expenseLoading: false, expensesError: null};
+
+export default (state = initialExpenses, action) => {
     switch(action.type) {
-        case ec.EXPENSE_REGISTER_REQUEST :
-            return {expenseLoading: true};                        
-        case ec.EXPENSE_SUCCESS_REQUEST :  
-            console.log("xxxxxx"+state.expenses);
-            return {expenseLoading: false, expenses: [...state.expenses, action.payload ]}; 
-        case ec.EXPENSE_FAIL_REQUEST :
-            console.log("Error happening is: " + action.payload);
-            return {expenseLoading: false, expensesError: action.payload };
-        
         case ec.FECTCH_EXPENSES_REQUEST :
-            return {loading: true, expenses: []};
+            return {loading: true, expenses: [] };
         case ec.FECTCH_EXPENSES_SUCCESS :
             return {loading: false, expenses: action.payload};
+
         case ec.FECTCH_EXPENSES_FAIL:
             return {loading: false, error: action.payload};
+
+        case ec.EXPENSE_REGISTER_REQUEST :
+            return { expenseLoading: true };  
+
+        case ec.EXPENSE_SUCCESS_REQUEST :
+            console.log("data from reducer", action.payload);
+              
+            return { expenseLoading: false, expenses: [...state.expenses, action.payload] }; 
+            
+        case ec.EXPENSE_FAIL_REQUEST :
+            
+            return {expenseLoading: false, expensesError: action.payload };
         default: 
             return state;
     }
